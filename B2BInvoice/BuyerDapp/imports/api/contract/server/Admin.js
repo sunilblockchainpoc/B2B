131c5352ea23e4dd2d63e38c150744c7de5838d0
sunilblockchainpoc <sunilkumar.gunasekaran@itcinfotech.com>
2018-03-02 12:54:38 +0530

    push changes

--- B2BInvoice/BuyerDapp/imports/api/contract/server/Admin.js
+++ B2BInvoice/BuyerDapp/imports/api/contract/server/Admin.js
@@ -5,8 +5,8 @@ var Web3 = require('web3');
 var Future = require('fibers/future');
 var Enum = require('enum');
 
-var rolesEnum = new Enum({'MunichRe':1,'CIC':2,'GeoTab':3,'MobileEye':4,'Installer':5,'BillItNow':6})
-var groupsEnum = new Enum({'MunichReGroup':1,'CICGroup':2,'GeoTabGroup':3,'MobileEyeGroup':4,'InstallerGroup':5,'BillItNowGroup':6})
+var rolesEnum = new Enum({'Participant1':1,'Participant2':2,'Participant3':3,'Participant4':4,'Participant5':5,'Participant6':6})
+var groupsEnum = new Enum({'Group1':1,'Group2':2,'Group3':3,'Group4':4,'Group5':5,'Group6':6})
 var statusEnum = new Enum ({'Active':0, 'Inactive':1});
 
 var const_gas = 4700000;
@@ -17,53 +17,28 @@ if (typeof web3 !== 'undefined') {
   web3 = new Web3(new Web3.providers.HttpProvider(Meteor.settings.server.web3api));
 }
 
-var RFQContractAddr = Meteor.settings.server.RFQContractAddress;
-var rolesRepositoryContractAddr = Meteor.settings.server.RolesRepositoryContractAddress;
-var userRepositoryContractAddr = Meteor.settings.server.UserRepositoryContractAddress;
-
-const RFQContractABI = require('../../../startup/server/Contract/RFQContract.json').abi;
-const RFQContractByteCode = "0x" + require('../../../startup/server/Contract/RFQContract.json').bytecode;
-var RFQContractInstance = web3.eth.contract(JSON.parse(RFQContractABI)).at(RFQContractAddr);
-
 const rolesRepositoryContractABI  = require('../../../startup/server/Contract/RolesRepository.json').abi;
 const rolesRepositoryContractByteCode  = '0x' + require('../../../startup/server/Contract/RolesRepository.json').bytecode;
-var rolesRepositoryContractInstance = web3.eth.contract(JSON.parse(rolesRepositoryContractABI)).at(rolesRepositoryContractAddr);
 
 const userRepositoryContractABI  = require('../../../startup/server/Contract/UserRepository.json').abi;
 const userRepositoryContractByteCode  = '0x' + require('../../../startup/server/Contract/UserRepository.json').bytecode;
-var userRepositoryContractInstance = web3.eth.contract(JSON.parse(userRepositoryContractABI)).at(userRepositoryContractAddr);
+
+const RFQContractABI = require('../../../startup/server/Contract/RFQContract.json').abi;
+const RFQContractByteCode = "0x" + require('../../../startup/server/Contract/RFQContract.json').bytecode;
+
+
+const POContractABI = require('../../../startup/server/Contract/POContract.json').abi;
+const POContractByteCode = "0x" + require('../../../startup/server/Contract/POContract.json').bytecode;
+
+const ShipmentContractABI = require('../../../startup/server/Contract/ShipmentContract.json').abi;
+const ShipmentContractByteCode = "0x" + require('../../../startup/server/Contract/ShipmentContract.json').bytecode;
 
 var rolesRepositorytransactionObject = {from: web3.eth.accounts[0],data: rolesRepositoryContractByteCode,gas:const_gas};
 var userRepositoryTransactionObject =  {from: web3.eth.accounts[0],data: userRepositoryContractByteCode,gas:const_gas};
-
+var rolesRepositoryContractAddr = Meteor.settings.server.RolesRepositoryContractAddress;
 
 Meteor.methods({
-
-    "createRFQContract": function(){ 
-      console.log("Creating RFQ Contract")
-        var contract = web3.eth.contract(JSON.parse(RFQContractABI));
-        var transactionObject = {from: web3.eth.accounts[0],data: RFQContractByteCode,gas:const_gas};
-        web3.eth.estimateGas(transactionObject,function(err,estimateGas){
-          if(!err)
-            transactionObject.gas = estimateGas * 2;
-        });
-        var future = new Future();
-        contract.new(transactionObject,function(error,result){
-          if(error) {
-            console.log(error);
-            future.return( error );
-          }
-          if(result) {
-            if(typeof result.address !== 'undefined') {
-              RFQContractAddr = result.address;
-              console.log(RFQContractAddr);
-              future.return( result.address );
-            }
-          }
-        });
-        return future.wait();
-      },
-    
+  
       "createRolesRepository": function(){ 
         var rolesRepositoryContract = web3.eth.contract(JSON.parse(rolesRepositoryContractABI));
         var transactionObject = {from: web3.eth.accounts[0],data: rolesRepositoryContractByteCode,gas:const_gas};
@@ -80,7 +55,7 @@ Meteor.methods({
           if(result) {
             if(typeof result.address !== 'undefined') {
               rolesRepositoryContractAddr = result.address;
-              console.log(rolesRepositoryContractAddr);
+              console.log("Roles Repository Contract Address = "+ rolesRepositoryContractAddr );
               future.return( result.address );
             }
           }
@@ -104,7 +79,7 @@ Meteor.methods({
           if(result) {
             if(typeof result.address !== 'undefined') {
               userRepositoryContractAddr = result.address;
-              console.log(userRepositoryContractAddr);
+              console.log("User Repository Contract Address = "+ userRepositoryContractAddr );
               future.return( result.address );
             }
           }
@@ -112,6 +87,83 @@ Meteor.methods({
         return future.wait();
       },
 
+      "createRFQContract": function(){ 
+        console.log("Creating RFQ Contract")
+          var contract = web3.eth.contract(JSON.parse(RFQContractABI));
+          var transactionObject = {from: web3.eth.accounts[0],data:RFQContractByteCode,gas:const_gas};
+          web3.eth.estimateGas(transactionObject,function(err,estimateGas){
+            if(!err)
+              transactionObject.gas = estimateGas * 2;
+          });
+          var future = new Future();
+          contract.new(transactionObject,function(error,result){
+            if(error) {
+              console.log(error);
+              future.return( error );
+            }
+            if(result) {
+              if(typeof result.address !== 'undefined') {
+                RFQContractAddr = result.address;
+                console.log("RFQ Contract Address = "+ RFQContractAddr );
+                future.return( result.address );
+              }
+            }
+          });
+          return future.wait();
+        },
+
+        "createPurchaseOrderContract": function(){ 
+          console.log("Creating Purchase Order Contract")
+            var contract = web3.eth.contract(JSON.parse(POContractABI));
+            var transactionObject = {from: web3.eth.accounts[0],data: POContractByteCode,gas:const_gas};
+            web3.eth.estimateGas(transactionObject,function(err,estimateGas){
+              if(!err)
+                transactionObject.gas = estimateGas * 2;
+            });
+            var future = new Future();
+            contract.new(transactionObject,function(error,result){
+              if(error) {
+                console.log(error);
+                future.return( error );
+              }
+              if(result) {
+                if(typeof result.address !== 'undefined') {
+                  POContractAddr = result.address;
+                  console.log("Purchase Order Contract Address = "+ POContractAddr );
+                  future.return( result.address );
+                }
+              }
+            });
+            return future.wait();
+          },
+  
+                   
+          "createShipmentContract": function(){ 
+            console.log("Creating Shipment Contract")
+              var contract = web3.eth.contract(JSON.parse(ShipmentContractABI));
+              var transactionObject = {from: web3.eth.accounts[0],data: ShipmentContractByteCode,gas:const_gas};
+              web3.eth.estimateGas(transactionObject,function(err,estimateGas){
+                if(!err)
+                  transactionObject.gas = estimateGas * 2;
+              });
+              var future = new Future();
+              contract.new(transactionObject,function(error,result){
+                if(error) {
+                  console.log(error);
+                  future.return( error );
+                }
+                if(result) {
+                  if(typeof result.address !== 'undefined') {
+                    ShipmentContractAddr = result.address;
+                    console.log("Shipment Contract Address = "+ ShipmentContractAddr );
+                    future.return( result.address );
+                  }
+                }
+              });
+              return future.wait();
+            } ,    
+                 
+             
    
     "createUser":function(params){
 
@@ -128,13 +180,14 @@ Meteor.methods({
                     future.return(err);
                 }
                 if(result){
-                  console.log("result:"+result);
                   userAddedEvent.watch(function(error,eventResult){
                     if(eventResult){
                         if(eventResult.blockNumber>block && eventResult.args.firstName==params.FirstName && eventResult.args.lastName==params.LastName)
                             userAddedEvent.stopWatching();
-                            if (!future.isResolved())  
+                            if (!future.isResolved())  {
+                                console.log("User ID - "+params.UserName+ "is successfully created.")
                                 future.return(eventResult.args.success);
+                            }
                     }
                    })
                 }
@@ -162,8 +215,10 @@ Meteor.methods({
                             if(eventResult){
                                 if(eventResult.blockNumber>block)
                                      passwordChangedEvent.stopWatching();
-                                        if (!future.isResolved())  
-                                             future.return(eventResult.args.success);
+                                        if (!future.isResolved())  {
+                                          console.log("Password change successfull for User ID - "+params.UserName)
+                                          future.return(eventResult.args.success);
+                                        }
                             }
                            })
                         }

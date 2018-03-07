cfb8ffd7aac0e0665b81070bb82f30008d982e1c
sunilblockchainpoc <sunilkumar.gunasekaran@itcinfotech.com>
2018-03-07 09:26:24 +0530

    Removed unwanted files

--- B2BInvoice/ShipmentDapp/imports/startup/server/index.js
+++
@@ -1,5 +0,0 @@
-// Import server startup through a single index entry point
-
-import '../../api/contract/server/ClientRFQ.js';
-import '../../api/contract/server/WorkOrder.js';
-import './routes.js'
--- B2BInvoice/ShipmentDapp/imports/ui/layouts/header/loginheader.js
+++
@@ -1,3 +0,0 @@
-import './loginheader.html';
-
-
